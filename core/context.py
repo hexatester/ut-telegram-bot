a95d324a0a3c6219a076385b31015b125c19a78f
hexatester <revolusi147id@gmail.com>
2020-09-07 19:28:01 +0700

    refactor CoreContext

--- core/context.py
+++ core/context.py
@@ -20,28 +20,16 @@ class CoreContext(CallbackContext):
     def session(self) -> Session:
         return self._session
 
-    @session.setter
-    def session(self, sesion) -> None:
-        assert isinstance(sesion, Session)
-        self._session = sesion
-
     @property
     def user(self) -> User:
         return self._user
-
-    @user.setter
-    def user(self, user) -> None:
-        assert isinstance(user, User)
-        self._user = user
 
     @classmethod
     def from_data(cls, update: Update, context: CallbackContext,
                   session: Session, user: User) -> CoreContext:
         self = cls(context.dispatcher)
-        if session is not None and isinstance(session, Session):
-            self._session = session
-        if user is not None and isinstance(user, User):
-            self._user = user
+        self._session = session
+        self._user = user
 
         if update is not None and isinstance(update, Update):
             chat = update.effective_chat
@@ -51,5 +39,6 @@ class CoreContext(CallbackContext):
                 self._chat_data = context.dispatcher.chat_data[chat.id]
             if user:
                 self._user_data = context.dispatcher.user_data[user.id]
-            self._message = update.effective_message
+
+        self._message = update.effective_message
         return self
