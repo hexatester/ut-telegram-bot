37b98ca2ba0f75402f671c59102dedd93be166d6
hexatester <revolusi147id@gmail.com>
2020-09-07 17:54:33 +0700

    fix start

--- handlers/commands/start.py
+++ handlers/commands/start.py
@@ -5,11 +5,12 @@ from core.session import message_wrapper
 
 @message_wrapper
 def start(update: Update, context: CoreContext):
+    user = update.effective_user
     if context.user and not context.user.started:
         context.user.started = True
         context.session.commit()
     update.effective_message.reply_text(
-        f'Selamat datang {context.user.full_name}\n\n'
+        f'Selamat datang {user.full_name}\n\n'
         'Daftar Perintah\n'
         '/start - Memulai bot\n'
         '/baca - Baca buku\n'
