b01c0dcc5509d16f48cd61c65281570d766eeae8
hexatester <revolusi147id@gmail.com>
2020-09-07 15:54:29 +0700

    improve CoreContext

--- core/context.py
+++ core/context.py
@@ -1,18 +1,46 @@
 from sqlalchemy.orm import Session
+from telegram import Update
 from telegram.ext import CallbackContext, Dispatcher
 from core.models import User
 
 
 class CoreContext(CallbackContext):
-    def __init__(self, dispatcher: Dispatcher, session: Session, user: User):
+    def __init__(self, dispatcher: Dispatcher):
         super(CoreContext, self).__init__(dispatcher)
-        self._session = session
-        self._user = user
+        self._session = None
+        self._user = None
 
     @property
     def session(self) -> Session:
         return self._session
 
+    @session.setter
+    def session(self, sesion) -> None:
+        assert isinstance(sesion, Session)
+        self._session = sesion
+
     @property
     def user(self) -> User:
         return self._user
+
+    @user.setter
+    def user(self, user) -> None:
+        assert isinstance(user, User)
+        self._user = user
+
+    @classmethod
+    def from_data(cls, update: Update, context: CallbackContext,
+                  session: Session, user: User):
+        self = cls(context.dispatcher)
+        self._session = session
+        self._user = user
+
+        if update is not None and isinstance(update, Update):
+            chat = update.effective_chat
+            user = update.effective_user
+
+            if chat:
+                self._chat_data = context.dispatcher.chat_data[chat.id]
+            if user:
+                self._user_data = context.dispatcher.user_data[user.id]
+        return self
